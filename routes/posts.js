95cbfd0813dc233a9bb177b8c99cebaba5287264
harsh <harshdeep1043@gmail.com>
2024-05-30 14:04:04 +0530

    changed delte post route

--- routes/posts.js
+++ routes/posts.js
@@ -41,24 +41,48 @@ router.put("/:id", async (req, res) => {
 });
 
 
-//DELETE POST
+//DELETE POST OLD
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const post = await Post.findById(req.params.id);
+//     if (post.username === req.body.username) {
+//       try {
+//         await post.delete();
+//         res.status(200).json("Post has been deleted...");
+//       } catch (err) {
+//         res.status(500).json(err);
+//       }
+//     } else {
+//       res.status(401).json("You can delete only your post!");
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+//DELTE ROUTE NEW 
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json("Post not found");
+    }
     if (post.username === req.body.username) {
       try {
-        await post.delete();
+        await Post.findByIdAndDelete(req.params.id);
         res.status(200).json("Post has been deleted...");
       } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json(err.message || "Internal Server Error");
       }
     } else {
       res.status(401).json("You can delete only your post!");
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message || "Internal Server Error");
   }
 });
+
+
 
 //GET POST
 router.get("/:id", async (req, res) => {
